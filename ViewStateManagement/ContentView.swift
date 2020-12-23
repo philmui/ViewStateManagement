891313ac2ae538b9c4f0f61b556577347a5e3ef9
Theodore Mui <theodoremui@gmail.com>
2020-12-23 15:36:38 -0800

    Working version of SwiftUI View Statement Management skeleton app

--- ViewStateManagement/ContentView.swift
+++ ViewStateManagement/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var theTask = Task(name: "Check all windows", isComplete: false, lastCompleted: nil)
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                // Checkmark if complete
+                Image(systemName: theTask.isComplete ?  "checkmark.square" : "square")
+                Text(theTask.name)
+            }
+            
+            ControlPanel(theTask: self.$theTask)
+        }
     }
 }
+
+struct ControlPanel: View {
+    @Binding var theTask: Task
+    
+    var body: some View {
+        if !theTask.isComplete {
+            Button("Complete", action: {
+                theTask.isComplete = true
+            }).padding(.top)
+        } else {
+            Button("Reset", action: {
+                theTask.isComplete = false
+            }).padding(.top)
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
---
+++ ViewStateManagement/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  ViewStateManagement
+//
+//  Created by Phil Mui on 12/23/20.
+//
+
+import Foundation
+
+struct Task {
+    var name: String = ""
+    var isComplete: Bool = false
+    var lastCompleted: Date?
+}

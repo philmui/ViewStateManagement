f8d3f3110f60abfedf07ac689065e8e7db535f66
Theodore Mui <theodoremui@gmail.com>
2020-12-23 14:59:07 -0800

    Initial Commit

---
+++ ViewStateManagement/ViewStateManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewStateManagementApp.swift
+//  ViewStateManagement
+//
+//  Created by Phil Mui on 12/23/20.
+//
+
+import SwiftUI
+
+@main
+struct ViewStateManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
